f81ef75126c73b81ad0132702b1fd00c96961733
Siang Lee, Khaw <khawslee@gmail.com>
2023-12-05 19:03:50 +0800

    Init flutter project

---
+++ android/app/src/main/java/com/example/inappwebview/inappwebview_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.inappwebview.inappwebview_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("In App Webview Flutter example"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+
+          },
+          child: Text("Open Webview"),
+        ),
+      ),
+    );
+  }
+}
